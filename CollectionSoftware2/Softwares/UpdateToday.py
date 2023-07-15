003c871f927d8507c1abe40c74b94cff4996ab83
Sasikaran2k3 <sasikaran2k3@gmail.com>
2023-07-15 23:28:06 +0530

    Proper UpdateToday

--- CollectionSoftware2/Softwares/UpdateToday.py
+++ CollectionSoftware2/Softwares/UpdateToday.py
@@ -3,7 +3,7 @@ import openpyxl
 from openpyxl import load_workbook, Workbook
 
 date = "".join(str(datetime.date.today()).split("-"))
-date = "20230712"
+
 try:
     wb = load_workbook(date + ".xlsx")
     pointer = wb.active
@@ -20,16 +20,33 @@ for i in range(len(list_of_shop)):
         dates = add_pointer["A"]
         amts = add_pointer["B"]
         max_val = len(amts)
-        print(max_val,list_of_shop[i].value)
+        for j in range(1,len(dates)+1):
+            cell = add_pointer.cell(row=j,column=1)
+            if cell.value == None:
+                add_pointer.cell(row = j, column=1).value = str(datetime.date.today())
+                add_pointer.cell(row=j, column=2).value = list_of_amt[i].value
+                break
+            elif str(datetime.date.today()) in cell.value:
+                add_pointer.cell(row=j,column=2).value = list_of_amt[i].value
+                break
+
+        else:
+            add_pointer.cell(row=j+1, column=1).value = str(datetime.date.today())
+            add_pointer.cell(row=j+1, column=2).value = list_of_amt[i].value
+        """
         if dates[0].value == None:
+            print("if")
             add_pointer["A1"].value =str(datetime.date.today())
             add_pointer["B1"].value = list_of_amt[i].value
         elif str(datetime.date.today()) in dates[-1].value:
+            print("elif")
             add_pointer["B%d"%max_val].value = list_of_amt[i].value
         else:
+            print("else")
             add_pointer["A%d" % max_val+1].value = str(datetime.date.today())
-            add_pointer["B%d" % max_val+1].value = list_of_amt[i].value
+            add_pointer["B%d" % max_val+1].value = list_of_amt[i].value"""
         #add_pointer.append((str(datetime.date.today()), list_of_amt[i].value))
         add_wb.save(list_of_shop[i].value + ".xlsx")
     except Exception as e:
+        print(list_of_shop[i].value)
         print(e)
